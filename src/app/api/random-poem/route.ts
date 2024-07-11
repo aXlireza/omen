4ab0fac4e58a41ee9766b2c28feeda55e9fecf69
aXlireza <alireza.ataee01@gmail.com>
2024-07-12 03:25:39 +0330

    using a random generating module

--- src/app/api/random-poem/route.ts
+++ src/app/api/random-poem/route.ts
@@ -1,4 +1,3 @@
-// app/api/read-csv/route.ts
 import { NextRequest, NextResponse } from 'next/server';
 import fs from 'fs';
 import path from 'path';
@@ -9,38 +8,36 @@ type PoemData = {
   interpreted: string[];
 };
 
+// Promisify the Papa Parse function
+const parseCSV = (fileContent: string) => {
+  return new Promise<Papa.ParseResult<any>>((resolve, reject) => {
+    Papa.parse(fileContent, {
+      header: true,
+      complete: (results) => resolve(results),
+      error: (error) => reject(error),
+    });
+  });
+};
+
 export async function GET(req: NextRequest) {
   try {
     const csvFilePath = path.resolve('./public/poems.csv');
     const fileContent = fs.readFileSync(csvFilePath, 'utf8');
 
-    let randomPoem: PoemData | null = null;
+    const results = await parseCSV(fileContent);
 
-    Papa.parse(fileContent, {
-      header: true,
-      complete: async (results: Papa.ParseResult<any>) => {
-        // console.log(results.data.length);
-        
-        const poems = results.data.slice(0, results.data.length-1).map((row: any, index:number) => {
-          return ({
-            poem: JSON.parse(row.Poem.replace(/'/g, '"')),
-            interpreted: JSON.parse(row.Interpreted.replace(/'/g, '"')),
-          })
-        });
-
-        const response = await fetch(`https://www.random.org/integers/?num=1&min=0&max=${results.data.length}&col=1&base=10&format=plain&rnd=new`, { cache: 'no-store' });
-        const randomValue = await response.text();
-
-        // const therandom = Math.floor(Number(randomValue.trim()) * poems.length)
-        console.log(Number(randomValue.trim()));
-        randomPoem = poems[Number(randomValue.trim())];
-      },
-      error: (error: { message: any; }) => {
-        return NextResponse.json({ error: error.message }, { status: 500 });
-      },
+    const poems = results.data.slice(0, results.data.length - 1).map((row: any) => {
+      return {
+        poem: JSON.parse(row.Poem.replace(/'/g, '"')),
+        interpreted: JSON.parse(row.Interpreted.replace(/'/g, '"')),
+      };
     });
-    // console.log(randomPoem);
-    
+
+    const response = await fetch(`https://www.random.org/integers/?num=1&min=0&max=${poems.length - 1}&col=1&base=10&format=plain&rnd=new`, { cache: 'no-store' });
+    const randomValue = await response.text();
+
+    const randomPoem = poems[Number(randomValue.trim())];
+
     return NextResponse.json(randomPoem);
   } catch (error) {
     return NextResponse.json({ error: (error as Error).message }, { status: 500 });
