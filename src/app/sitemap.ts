4b521658603c746d05d50f4fe4b5812cb18c158f
aXlireza <alireza.ataee01@gmail.com>
2024-07-27 12:37:06 +0330

    fixing the priority numbers

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -1,21 +1,25 @@
 import { parseCSV } from '@/lib/csvParser';
 import { MetadataRoute } from 'next'
- 
+
 export default function sitemap(): MetadataRoute.Sitemap {
   const poems = parseCSV();
-  const poemsSitemap = poems.map((data, index) => ({
-    url: `https://omen-seven.vercel.app/poem/${index}`,
-    lastModified: new Date(),
-    // changeFrequency: 'monthly',
-    priority: 2,
-  }))
   return [
     {
       url: 'https://omen-seven.vercel.app/',
       lastModified: new Date(),
       changeFrequency: 'daily',
       priority: 1,
+    },{
+      url: 'https://omen-seven.vercel.app/poem/',
+      lastModified: new Date(),
+      // changeFrequency: 'daily',
+      priority: .8,
     },
-    ...poemsSitemap
+    ...poems.map((data, index) => ({
+      url: `https://omen-seven.vercel.app/poem/${index}`,
+      lastModified: new Date(),
+      // changeFrequency: 'monthly',
+      priority: .5,
+    }))
   ]
 }
